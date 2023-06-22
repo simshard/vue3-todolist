9b2422ffa1862d86a9413286ff996910d3ee4108
simshard <30438951+simshard@users.noreply.github.com>
2023-06-22 22:55:43 +0100

    json server

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -13,12 +13,15 @@ export default {
 
     data() {
         return {
-            assignments: [
-                { name: 'Finish project', complete: false, id: 1 ,tag:'maths'},
-                { name: 'Read Chapter 4', complete: false, id: 2 ,tag:'science'},
-                { name: 'Turn in Homework ', complete: false, id: 3 ,tag:'maths'},
-            ]
+            assignments:  []
         }
+    },
+    created(){
+        fetch('http://localhost:3001/assignments')
+        .then(response => response.json())
+            .then(assignments =>{
+                this.assignments = assignments;
+            });
     },
 
     computed: {
